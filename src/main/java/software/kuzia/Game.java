ec367d8c6ced588c6294e1d2a22b61a19ea4798a
Кузьмин Евгений <eugene.kuzmin@live.ru>
2022-12-29 12:41:46 +0300

    asteroid-belt

--- src/main/java/software/kuzia/Game.java
+++ src/main/java/software/kuzia/Game.java
@@ -3,6 +3,8 @@ package software.kuzia;
 public class Game {
     public static void main(String[] args) {
         Belt belt = new Belt(101);
+        Mover mover = new Mover(belt.getSize() / 2 - 1, belt.getSize() / 2, 1);
+        Radar radar = new Radar();
 
         for (int r = 44; r < 57; r++) {
             belt.setAsteroid(52, r, 'O');
@@ -16,28 +18,18 @@ public class Game {
 
         belt.setAsteroid(belt.getSize() / 2, belt.getSize() / 2, 'X');
 
-        Mover mover = new Mover(belt.getSize() / 2 - 1, belt.getSize() / 2, 1);
-        Radar radar;
+        radar.draw(belt, mover);
 
-        radar = new Radar(belt, mover);
-        radar.draw();
+        for (int i = 0; i < 4; i++) {
+            mover.turnRight();
+            radar.draw(belt, mover);
+        }
 
-        mover.turnRight();
+        for (int i = 0; i < 4; i++) {
+            mover.turnLeft();
+            radar.draw(belt, mover);
+        }
 
-        radar = new Radar(belt, mover);
-        radar.draw();
-
-        mover.turnRight();
-
-        radar = new Radar(belt, mover);
-        radar.draw();
-
-        mover.turnRight();
-
-        radar = new Radar(belt, mover);
-        radar.draw();
-
-        // mover.turnLeft();
         // System.out.println(belt.getAsteroid(mover.getMoveLine(),mover.getMoveRow()));
     }
 }
